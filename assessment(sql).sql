e54cc807754d63a640f03f1c83d7c9b03547dcef
bansi-vamja <113992554+bansi-vamja@users.noreply.github.com>
2023-11-11 17:55:40 +0530

    Add files via upload

--- assessment(sql).sql
+++ assessment(sql).sql
@@ -21,38 +21,42 @@ insert into worker(worker_id,s_first_name,s_last_name,salary,joining_date,depart
 
 select * from worker;
 
--- queury 1--  
-select worker_id,s_first_name,s_last_name,salary,joining_date,department from worker order by (s_first_name)asc ;
-select worker_id,s_first_name,s_last_name,salary,joining_date,department from worker order by (department)desc ;
+-- queury 1. Write an SQL query to print all Worker details from the Worker table order by FIRST_NAME Ascending and DEPARTMENT Descendin.--  
+select worker_id,s_first_name,s_last_name,salary,joining_date,department from worker order by s_first_name asc,department desc ;
 
--- queury 2--  
+
+-- queury 2.Write an SQL query to print details for Workers with the first names “Vipul” and “Satish” from the Worker table. --  
 select  worker_id,s_first_name,s_last_name,salary,joining_date,department from worker where s_first_name in ('vipul', 'satish');
 
--- queury 3-- 
+-- queury 3 Write an SQL query to print details of the Workers whose FIRST_NAME ends with ‘h’ and contains six alphabets. -- 
 
 select * from worker where s_first_name like '_____h%';
 
--- queury 4--
+-- queury 4 Write an SQL query to print details of the Workers whose SALARY lies between 1. --
     
     select salary from  worker where salary between 200000 and 500000;
- -- queury 5-- 
+ -- queury 5 Write an SQL query to fetch duplicate records having matching data in some fields of a table. -- 
  
  select worker_id,s_first_name,s_last_name,salary,count(*) as duplicate from worker group by  worker_id having count(*)>1;
  select * from worker;
  
- -- queury 6-- 
+ -- queury 6  Write an SQL query to show the top 6 records of a table. -- 
  
  select  worker_id,s_first_name,s_last_name,salary,joining_date,department  from worker limit 6; 
  
-  -- queury 7--
+  -- queury 7 Write an SQL query to fetch the departments that have less than five people in them. --
  
- select department from worker group by department having department <5;
+
+ SELECT department, COUNT(worker_id) AS num_people
+FROM worker
+GROUP BY department
+HAVING num_people < 5;
  
- -- queury 8--
+ -- queury 8 Write an SQL query to show all departments along with the number of people in there. --
   
   select department ,count(department) from worker group by department;
  
- -- queury 9-- 
+ -- queury 9 Write an SQL query to print the name of employees having the highest salary in each department.-- 
  
  select s_first_name , department from worker where salary in (select max(salary) from worker group by department);
  
@@ -60,7 +64,7 @@ select * from worker where s_first_name like '_____h%';
  /*ASSESMENT 2--*/
  
  use xyz;
-create table student 
+ create table student 
 (
 std_id int,
 std_name varchar(20),
@@ -88,22 +92,23 @@ insert into student (std_id,std_name,sex,percentage,class,sec,stream_,dob)values
 (1013,'akriti saxena','female',89.00,12,'a','science','2008/11/23'),
 (1014,'tani rastogi','female',82.00,12,'a','science','2008/11/23');
 
+/*que=1To display all the records form STUDENT table. SELECT * FROM student ; */
+
 select * from student;
-delete from student where std_id=1003;
 
- update student set stream_='arts' where std_id=1006;
-
--- queury  2 -- 
+-- queury  2  To display any name and date of birth from the table STUDENT. SELECT StdName, DOB FROM student ; 
+-- 
 
 select std_name ,dob from student;
 
--- queury  3 --
+-- queury  3 . To display all students record where percentage is greater of equal to 80 FROM student table. --
+SELECT * FROM student WHERE percentage >= 80;  
 
-select std_name,percentage from student where percentage >=80; 
 
--- queury  4 --
+-- queury  4 To display student name, stream and percentage where percentage of student is more than 80 --
+
 select std_name,stream_,percentage from student where percentage >80;
 
--- queury  5 --
+-- queury  5  To display all records of science students whose percentage is more than 75 form student table.-- 
 
 select * from student where stream_ = 'science' and percentage > 75;
